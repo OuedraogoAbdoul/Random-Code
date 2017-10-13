4de4fa276590e4a9541d0c873a04a6a6f0f9ed4c
Abdoul Ouedraogo <ouedraogo.abdoul.w@gmail.com>
2017-10-13 01:55:53 -0700

    Student information code

---
+++ StudentData/StudentData/Program.cs
@@ -0,0 +1,22 @@
+﻿//This program dislay a student name, graduation date
+//The program also display the school name
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentData
+{
+    class StudentInfo
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First Name: Peter \n");
+            Console.WriteLine("Last Name: Smith\n");
+            Console.WriteLine("School Name: Free University\n");
+            Console.ReadKey();
+        }
+    }
+}
