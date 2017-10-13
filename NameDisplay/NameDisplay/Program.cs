9330dc98a465c8257ed09db4830c93fb035c93f6
Abdoul Ouedraogo <ouedraogo.abdoul.w@gmail.com>
2017-10-13 01:39:16 -0700

    New Csharp program

---
+++ NameDisplay/NameDisplay/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameDisplay
+{
+    class MainClass
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! My name is Peter ");
+            Console.ReadKey();
+        }
+    }
+}
